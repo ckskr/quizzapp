de607400888274f1427f7d09cb34a7c6ee75ce39
C K <cl-ki@web.de>
2022-07-05 20:42:36 +0200

    Add answer toggle

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
+//Answer
+
 let button = document.querySelector('[data-js="button"]');
-
-let text = document.querySelector('[data-js="text"]');
-
-console.log(button);
-console.log(text);
+let answer = document.querySelector('[data-js="answer"]');
 
 function onButtonClick() {
-  console.log("button clicked!");
-
-  text.classList.toggle("text--highlight");
+  answer.classList.toggle("card__answer__shown");
 }
 
 button.addEventListener("click", onButtonClick);
+
+//Bookmark
+
+let bookmark = document.querySelector('[data-js="bookmark"]');
+
+let fill = document.querySelector('[data-js="filled"]');
+
+function clickBookmark() {
+  fill.classList.toggle("bookmark__icon_clicked");
+}
+
+bookmark.addEventListener("click", clickBookmark);
