40c3e19b8f1b98d10dbc32ddee027600fd11f13d
C K <cl-ki@web.de>
2022-07-06 19:54:47 +0200

    add js to bookmark and answerbutton

---
+++ components/questioncard/questioncard.js
@@ -0,0 +1,18 @@
+export default function Questioncard() {
+  const button = document.querySelector('[data-js="button"]');
+  const answer = document.querySelector('[data-js="answer"]');
+
+  let isAnswerShown = false;
+
+  button.addEventListener("click", () => {
+    isAnswerShown = !isAnswerShown;
+
+    if (isAnswerShown) {
+      button.innerText = "Hide answer";
+      answer.classList.add("card__answer__shown");
+    } else {
+      button.innerText = "Show answer";
+      answer.classList.remove("card__answer__shown");
+    }
+  });
+}
--- main.js
+++ main.js
@@ -1,16 +1,4 @@
-//Bookmark
-
 import Bookmark from "./components/bookmark/bookmark.js";
-
+import Questioncard from "./components/questioncard/questioncard.js";
 Bookmark();
-
-//Answer
-
-const button = document.querySelector('[data-js="button"]');
-const answer = document.querySelector('[data-js="answer"]');
-
-function onButtonClick() {
-  answer.classList.toggle("card__answer__shown");
-}
-
-button.addEventListener("click", onButtonClick);
+Questioncard();
